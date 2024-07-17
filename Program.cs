9db3eb2a09b83bbac0cfe62487b0e0f25143569a
Sanjay Subrahmanya Bhat <sanq1kor@bosch.com>
2024-07-17 11:49:47 +0530

    commit

--- Program.cs
+++ Program.cs
@@ -58,20 +58,17 @@ namespace TelCo.ColorCoder
             int pairNumber = 4;
             ColorPair testPair1 = Program.GetColorFromPairNumber(pairNumber);
             Console.WriteLine("[In]Pair Number: {0},[Out] Colors: {1}\n", pairNumber, testPair1);
-            Debug.Assert(testPair1.majorColor == Color.White);
-            Debug.Assert(testPair1.minorColor == Color.Brown);
+            Debug.Assert(testPair1.majorColor == Color.White);Debug.Assert(testPair1.minorColor == Color.Brown);
 
             pairNumber = 5;
             testPair1 = Program.GetColorFromPairNumber(pairNumber);
             Console.WriteLine("[In]Pair Number: {0},[Out] Colors: {1}\n", pairNumber, testPair1);
-            Debug.Assert(testPair1.majorColor == Color.White);
-            Debug.Assert(testPair1.minorColor == Color.SlateGray);
+            Debug.Assert(testPair1.majorColor == Color.White);Debug.Assert(testPair1.minorColor == Color.SlateGray);
 
             pairNumber = 23;
             testPair1 = Program.GetColorFromPairNumber(pairNumber);
             Console.WriteLine("[In]Pair Number: {0},[Out] Colors: {1}\n", pairNumber, testPair1);
-            Debug.Assert(testPair1.majorColor == Color.Violet);
-            Debug.Assert(testPair1.minorColor == Color.Green);
+            Debug.Assert(testPair1.majorColor == Color.Violet);Debug.Assert(testPair1.minorColor == Color.Green);
 
             ColorPair testPair2 = new ColorPair() { majorColor = Color.Yellow, minorColor = Color.Green };
             pairNumber = Program.GetPairNumberFromColor(testPair2);
